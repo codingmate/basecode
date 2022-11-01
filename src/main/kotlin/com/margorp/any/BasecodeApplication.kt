9c216d7f2df105566372bdda7247cb5b0fc2bad6
계정 <hsykys0728@gmail.com>
2022-11-01 23:40:03 +0900

    First Commit

---
+++ src/main/kotlin/com/margorp/any/BasecodeApplication.kt
@@ -0,0 +1,12 @@
+package com.margorp.any
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class BasecodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<BasecodeApplication>(*args)
+}
