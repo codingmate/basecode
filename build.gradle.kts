6e6eb1ba2899932e6dd44d6003e6c0203eea48d5
계정 <hsykys0728@gmail.com>
2022-11-03 00:31:01 +0900

    add mockito dependency <- occur MockMvc Error(final class...)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.mockito:mockito-inline:4.8.0")
 }
 
 tasks.withType<KotlinCompile> {
